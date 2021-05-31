805c5b6670660f3b2f28bd2983454d1d4e8d36f6
iamspd <iamspd.ai9@gmail.com>
2021-05-30 21:09:02 -0400

    MainActivity - Methods implemented

--- app/src/main/java/com/example/showandhide/MainActivity.java
+++ app/src/main/java/com/example/showandhide/MainActivity.java
@@ -3,12 +3,29 @@ package com.example.showandhide;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    // widgets
+    private TextView tvHello;
+
+    // methods
+    public void onClickHide(View view) {
+        tvHello.setVisibility(View.INVISIBLE);
+    }
+
+    public void onClickShow(View view) {
+        tvHello.setVisibility(View.VISIBLE);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tvHello = findViewById(R.id.tvHello);
     }
 }
